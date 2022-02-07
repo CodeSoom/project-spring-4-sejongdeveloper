1ddee636bddc77e50f83c48b6580ce8193bbba8b
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-02-07 21:15:19 +0900

    [추가] 출고 등록 컨트롤러 테스트 추가

---
+++ sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/controllers/ReleaseOrderController.java
@@ -0,0 +1,17 @@
+package com.codesoom.sejongdeveloper.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 출고에 대한 요청을 관리한다.
+ */
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/release-orders")
+public class ReleaseOrderController {
+
+
+
+}
---
+++ sejongdeveloper/src/test/java/com/codesoom/sejongdeveloper/controllers/ReleaseOrderControllerTest.java
@@ -0,0 +1,33 @@
+package com.codesoom.sejongdeveloper.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SuppressWarnings({"InnerClassMayBeStatic", "NonAsciiCharacters"})
+@DisplayName("ReleaseOrderController 클래스")
+@WebMvcTest(ReleaseOrderController.class)
+class ReleaseOrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Nested
+    class 출고저장_요청을_처리하는_핸들러는 {
+        @Nested
+        class 유효성_검사를_통과한_경우 {
+            @Test
+            void 회원을_저장한다() throws Exception {
+                mockMvc.perform(get("/release-orders"))
+                        .andExpect(status().isOk());
+            }
+        }
+    }
+
+}
