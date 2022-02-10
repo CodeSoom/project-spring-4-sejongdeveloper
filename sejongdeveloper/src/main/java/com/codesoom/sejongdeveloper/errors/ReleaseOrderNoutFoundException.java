0a74eeeaa96e918de4cf69d8726fdd12e2b77c44
SeJongDeveloper <51711799+sejongdeveloper@users.noreply.github.com>
2022-02-10 21:51:08 +0900

    Revert "출고상세 조회요청 컨트롤러"

--- sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/errors/ReleaseOrderNoutFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.codesoom.sejongdeveloper.errors;
-
-public class ReleaseOrderNoutFoundException extends RuntimeException {
-    public ReleaseOrderNoutFoundException(Long id) {
-        super(String.format("[%d] 출고를 찾을 수 없습니다.\n" +
-                "출고의 일련번호를 확인하세요.", id));
-    }
-}
