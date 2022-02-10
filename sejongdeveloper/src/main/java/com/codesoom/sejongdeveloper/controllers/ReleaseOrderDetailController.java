c818cf8eabad0bfd8ad1e80234c5d1f2145ec6d3
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-02-10 22:48:59 +0900

    [수정] 출고상세 조회 컨트롤러 테스트 구현

--- sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/controllers/ReleaseOrderDetailController.java
+++ sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/controllers/ReleaseOrderDetailController.java
@@ -1,14 +1,22 @@
 package com.codesoom.sejongdeveloper.controllers;
 
+import com.codesoom.sejongdeveloper.dto.ReleaseOrderDetailResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/release-order-details")
 public class ReleaseOrderDetailController {
 
-
+    @GetMapping("/release-orders/{id}")
+    public List<ReleaseOrderDetailResponse> list(@PathVariable Long id) {
+        return null;
+    }
 
 }
