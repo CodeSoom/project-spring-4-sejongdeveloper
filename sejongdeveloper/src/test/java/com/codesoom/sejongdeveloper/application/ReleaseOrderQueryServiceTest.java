89a68d7c3a6610bd1ffdfad1a2f1ba2e43048a2e
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-02-13 11:55:51 +0900

    [수정] 출고목록조회 쿼리용 서비스 실패 테스트

--- sejongdeveloper/src/test/java/com/codesoom/sejongdeveloper/application/ReleaseOrderQueryServiceTest.java
+++ sejongdeveloper/src/test/java/com/codesoom/sejongdeveloper/application/ReleaseOrderQueryServiceTest.java
@@ -1,5 +1,11 @@
 package com.codesoom.sejongdeveloper.application;
 
+import com.codesoom.sejongdeveloper.controllers.ReleaseOrderController;
+import com.codesoom.sejongdeveloper.domain.ReleaseOrder;
+import com.codesoom.sejongdeveloper.dto.ReleaseOrderResponse;
+import com.codesoom.sejongdeveloper.dto.ReleaseOrderSearchCondition;
+import com.codesoom.sejongdeveloper.repository.ReleaseOrderRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -7,7 +13,12 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
@@ -16,8 +27,13 @@ import org.springframework.transaction.annotation.Transactional;
 @DisplayName("ReleaseOrderQueryService 클래스")
 class ReleaseOrderQueryServiceTest {
 
+    private static final String RELEASE_ORDER_NAME = "출고명";
+
     @Autowired
     private ReleaseOrderQueryService releaseOrderQueryService;
+
+    @Autowired
+    private ReleaseOrderRepository releaseOrderRepository;
 
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -25,10 +41,29 @@ class ReleaseOrderQueryServiceTest {
         @Nested
         @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
         class 유효한_파라미터인_경우 {
+            private ReleaseOrderSearchCondition condition;
+            private Pageable pageable;
+
+            @BeforeEach
+            void setUp() {
+                ReleaseOrder releaseOrder = ReleaseOrder.builder()
+                        .name(RELEASE_ORDER_NAME)
+                        .build();
+
+                releaseOrderRepository.save(releaseOrder);
+
+                condition = new ReleaseOrderSearchCondition();
+                condition.setName(releaseOrder.getName());
+
+                pageable = PageRequest.of(0, 10);
+            }
+
             @Test
             @DisplayName("출고 목록을 리턴한다")
             void 출고_목록을_리턴한다() {
+                Page<ReleaseOrderResponse> page = releaseOrderQueryService.search(condition, pageable);
 
+                assertThat(page.getContent().size()).isEqualTo(1);
             }
         }
     }
