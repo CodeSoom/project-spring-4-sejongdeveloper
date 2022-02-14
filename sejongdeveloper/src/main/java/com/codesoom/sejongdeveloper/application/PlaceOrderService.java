51e274c00d611147d8ae275da870866541eff51e
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-02-14 22:11:57 +0900

    [수정] 발주등록 서비스 주석추가

--- sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/application/PlaceOrderService.java
+++ sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/application/PlaceOrderService.java
@@ -7,14 +7,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * 발주에 관하여 관리한다.
+ */
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
 public class PlaceOrderService {
 
-
     private final PlaceOrderRepository placeOrderRepository;
 
+    /**
+     * 주어진 발주를 저장하고 저장된 발주를 리턴한다.
+     *
+     * @param request 저장할 발주
+     * @return 저장된 발주
+     */
     public Long savePlaceOrder(PlaceOrderSaveRequest request) {
         PlaceOrder placeOrder = new PlaceOrder(request);
 
