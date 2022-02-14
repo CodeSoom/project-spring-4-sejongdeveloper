4f2b57c83335abf1b57846f61b04348ca9d87df8
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-02-14 22:31:11 +0900

    [수정] 발주등록 컨트롤러 성공 테스트

--- sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/controllers/PlaceOrderController.java
+++ sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/controllers/PlaceOrderController.java
@@ -1,12 +1,22 @@
 package com.codesoom.sejongdeveloper.controllers;
 
+import com.codesoom.sejongdeveloper.dto.PlaceOrderSaveRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/place-orders")
 public class PlaceOrderController {
+
+    @PostMapping
+    public Long save(@RequestBody @Valid PlaceOrderSaveRequest request) {
+        return null;
+    }
 
 }
--- sejongdeveloper/src/test/java/com/codesoom/sejongdeveloper/application/PlaceOrderServiceTest.java
+++ sejongdeveloper/src/test/java/com/codesoom/sejongdeveloper/application/PlaceOrderServiceTest.java
@@ -11,8 +11,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
