0167c9748be4e525279d79624c6a9058eec21e3d
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-02-20 12:09:57 +0900

    [추가] 수주목록 컨트롤러 실패 테스트 작성

--- sejongdeveloper/src/test/java/com/codesoom/sejongdeveloper/controllers/PlaceOrderControllerTest.java
+++ sejongdeveloper/src/test/java/com/codesoom/sejongdeveloper/controllers/PlaceOrderControllerTest.java
@@ -4,6 +4,7 @@ import com.codesoom.sejongdeveloper.application.PlaceOrderService;
 import com.codesoom.sejongdeveloper.domain.PlaceOrder;
 import com.codesoom.sejongdeveloper.dto.PlaceOrderDetailUpdateRequest;
 import com.codesoom.sejongdeveloper.dto.PlaceOrderSaveRequest;
+import com.codesoom.sejongdeveloper.dto.PlaceOrderSearchCondition;
 import com.codesoom.sejongdeveloper.dto.PlaceOrderUpdateRequest;
 import com.codesoom.sejongdeveloper.repository.PlaceOrderRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -17,15 +18,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -42,6 +47,7 @@ class PlaceOrderControllerTest {
     private static final Long INVALID_PLACE_ORDER_ID = 2L;
     private static final Long PLACE_ORDER_DETAIL_ID = 1L;
     private static final Long ITEM_ID = 1L;
+    private static final LocalDate PLACE_ORDER_DATE = LocalDate.now();
 
     @Autowired
     private MockMvc mockMvc;
@@ -233,6 +239,53 @@ class PlaceOrderControllerTest {
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(json))
                         .andExpect(status().isBadRequest());
+            }
+        }
+    }
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class 발주목록_조회를_요청하는_핸들러는 {
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 검색조건을_만족하는_발주목록을_찾은_경우 {
+            PlaceOrderSearchCondition nameCondition;
+            PlaceOrderSearchCondition dateCondition;
+
+            @BeforeEach
+            void setUp() {
+                Pageable pageable = PageRequest.of(0, 10);
+
+                nameCondition = PlaceOrderSearchCondition.builder()
+                        .name(PLACE_ORDER_NAME)
+                        .pageable(pageable)
+                        .build();
+
+                dateCondition = PlaceOrderSearchCondition.builder()
+                        .date(PLACE_ORDER_DATE)
+                        .pageable(pageable)
+                        .build();
+
+            }
+
+            @Test
+            @DisplayName("발주목록을 리턴한다")
+            void 발주목록을_리턴한다() throws Exception {
+                String json = objectMapper.writeValueAsString(nameCondition);
+
+                mockMvc.perform(get("/place-orders")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(json))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(containsString(PLACE_ORDER_NAME)));
+
+                json = objectMapper.writeValueAsString(dateCondition);
+
+                mockMvc.perform(get("/place-orders")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(json))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(containsString(PLACE_ORDER_DATE.toString())));
             }
         }
     }
