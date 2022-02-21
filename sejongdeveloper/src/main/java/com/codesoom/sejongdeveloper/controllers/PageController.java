9ec1ba08278a35f7eee24c52ea2c1a647bb00a70
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-02-21 20:49:36 +0900

    [추가] 수주목록 페이지 작성

---
+++ sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/controllers/PageController.java
@@ -0,0 +1,25 @@
+package com.codesoom.sejongdeveloper.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 페이지 요청에 대하여 관리한다.
+ */
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/page")
+public class PageController {
+
+    /**
+     * 수주목록 페이지로 이동한다.
+     *
+     * @return 수주목록 페이지
+     */
+    @GetMapping("/obtain-orders")
+    public String getObtainOrders() {
+        return "obtainOrders/obtainOrders";
+    }
+}
