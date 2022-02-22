f45f838d943b50b19f6c893b0c1a22ebeb0de8c5
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-02-22 21:11:33 +0900

    [수정] 수주상세 조회 페이지 작성

--- sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/controllers/PageController.java
+++ sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/controllers/PageController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -32,8 +33,9 @@ public class PageController {
      *
      * @return 수주상세 페이지
      */
-    @GetMapping("/obtain-order")
-    public String obtainOrder(Model model) {
+    @GetMapping({"/obtain-order", "/obtain-order/{id}"})
+    public String obtainOrder(@PathVariable(required = false) Long id, Model model) {
+        model.addAttribute("obtainOrderId", id);
         model.addAttribute("items", itemRepository.findAll());
 
         return "obtainOrders/obtainOrder";
