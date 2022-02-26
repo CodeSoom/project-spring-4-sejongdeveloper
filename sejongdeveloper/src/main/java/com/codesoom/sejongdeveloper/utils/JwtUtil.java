487758785ccba48a5fc28dfd543faa4bfbe553f3
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-02-26 11:45:08 +0900

    [수정] jwt 인코딩에 대한 null처리 성공 테스트 작성

--- sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/utils/JwtUtil.java
+++ sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/utils/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.codesoom.sejongdeveloper.utils;
 
+import com.codesoom.sejongdeveloper.errors.JwtInvalidException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,16 +9,21 @@ import java.nio.charset.StandardCharsets;
 import java.security.Key;
 
 public class JwtUtil {
-    private final Key key;
+    private final Key tokenKey;
 
     public JwtUtil(@Value("${jwt.key}") String key) {
-        this.key = Keys.hmacShaKeyFor(key.getBytes(StandardCharsets.UTF_8));
+        this.tokenKey = Keys.hmacShaKeyFor(key.getBytes(StandardCharsets.UTF_8));
     }
 
-    public String encode(String name, Object value) {
+    public String encode(String key, Object value) {
+        if (key == null || value == null) {
+            throw new JwtInvalidException(key, value);
+        }
+
         return Jwts.builder()
-                .claim(name, value)
-                .signWith(key)
+                .claim(key, value)
+                .signWith(tokenKey)
                 .compact();
     }
+
 }
