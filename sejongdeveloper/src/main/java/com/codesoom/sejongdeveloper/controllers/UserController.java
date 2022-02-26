52d53d56d4406097ff2d0e9fe29ec1deb16da6da
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-02-26 13:39:23 +0900

    [수정] 로그인에 대한 성공 테스트 작성

--- sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/controllers/UserController.java
+++ sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/controllers/UserController.java
@@ -1,8 +1,17 @@
 package com.codesoom.sejongdeveloper.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
+    @PostMapping("/login")
+    public String login(String loginId, String password) {
+        return null;
+    }
 }
