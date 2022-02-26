16a49853ffd4d7dcd32f954e12c049a41c1d52c6
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-02-26 16:02:59 +0900

    [수정] 주어진 파라미터가 없는 경우 로그인 성공 테스트 작성

--- sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/controllers/UserController.java
+++ sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/controllers/UserController.java
@@ -1,7 +1,6 @@
 package com.codesoom.sejongdeveloper.controllers;
 
 import com.codesoom.sejongdeveloper.application.UserService;
-import com.codesoom.sejongdeveloper.domain.User;
 import com.codesoom.sejongdeveloper.dto.LoginData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,7 +21,7 @@ public class UserController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/login")
-    public String login(@RequestBody LoginData loginData) {
+    public String login(@RequestBody @Valid LoginData loginData) {
         return userService.login(loginData.getLoginId(), loginData.getPassword());
     }
 }
--- sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/dto/LoginData.java
+++ sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/dto/LoginData.java
@@ -3,11 +3,15 @@ package com.codesoom.sejongdeveloper.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 public class LoginData {
 
+    @NotNull
     private String loginId; //로그인 아이디
 
+    @NotNull
     private String password;    //비밀번호
 
     @Builder
