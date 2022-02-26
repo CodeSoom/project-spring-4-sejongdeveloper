c92f3a508882371cb931897e4806e9b5b2f5ae58
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-02-27 03:36:19 +0900

    [추가] 인터셉터 추가

--- sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/config/WebConfig.java
+++ sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/config/WebConfig.java
@@ -3,6 +3,10 @@ package com.codesoom.sejongdeveloper.config;
 import com.codesoom.sejongdeveloper.application.UserService;
 import com.codesoom.sejongdeveloper.interceptors.LoginInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.http.LegacyCookieProcessor;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,7 +19,19 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor(userService))
-                .addPathPatterns("/page/**");
+        registry.addInterceptor(loginInterceptor(userService))
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/users/login", "/users/logout", "/error", "/favicon.ico");
+    }
+
+    @Bean
+    public LoginInterceptor loginInterceptor(UserService userService) {
+        return new LoginInterceptor(userService);
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
+        return (serverFactory) -> serverFactory.addContextCustomizers(
+                (context) -> context.setCookieProcessor(new LegacyCookieProcessor()));
     }
 }
