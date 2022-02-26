56da326c7ced40bafb6e43633e82373893c7ae87
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-02-27 04:01:53 +0900

    [수정] 유저 서비스 주석 추가

--- sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/application/UserService.java
+++ sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/application/UserService.java
@@ -9,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * 유저에 대하여 관리한다.
+ */
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
@@ -17,6 +20,14 @@ public class UserService {
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
 
+    /**
+     * 주어진 로그인 정보의 유저가 있는 경우 토큰을 리턴한다.
+     *
+     * @param loginId 유저의 로그인 아이디
+     * @param password 유저의 비밀번호
+     * @return 주어진 로그인 정보의 유저 토큰
+     * @throws UserNotFoundException 주어진 로그인 정보의 유저를 찾지 못한 경우
+     */
     public String login(String loginId, String password) {
         User user = userRepository.findByLoginIdAndPassword(loginId, password)
                 .orElseThrow(() -> new UserNotFoundException(loginId));
@@ -24,6 +35,13 @@ public class UserService {
         return jwtUtil.encode("userId", user.getId());
     }
 
+    /**
+     * 주어진 토큰의 유저가 잇는 경우 유저를 리턴한다.
+     *
+     * @param token 유저의 토큰
+     * @return 주어진 토큰의 유저
+     * @throws UserNotFoundException 주어진 토큰의 유저를 찾지 못한 경우
+     */
     public User findUser(String token) {
         Claims claims = jwtUtil.decode(token);
         Long id = claims.get("userId", Long.class);
