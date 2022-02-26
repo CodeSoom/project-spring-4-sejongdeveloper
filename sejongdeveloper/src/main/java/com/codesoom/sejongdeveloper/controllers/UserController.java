347b9b464a39061582bbfeffe70e3929c1c7d27d
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-02-27 03:44:52 +0900

    [수정] 로그아웃 컨트롤러 성공 테스트 작성

--- sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/controllers/UserController.java
+++ sejongdeveloper/src/main/java/com/codesoom/sejongdeveloper/controllers/UserController.java
@@ -4,13 +4,18 @@ import com.codesoom.sejongdeveloper.application.UserService;
 import com.codesoom.sejongdeveloper.dto.LoginData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,5 +28,13 @@ public class UserController {
     @PostMapping("/login")
     public String login(@RequestBody @Valid LoginData loginData) {
         return userService.login(loginData.getLoginId(), loginData.getPassword());
+    }
+
+    @GetMapping("/logout")
+    public void logout(HttpServletResponse response) throws IOException {
+        Cookie cookie = new Cookie("Authentication", null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        response.sendRedirect("/");
     }
 }
